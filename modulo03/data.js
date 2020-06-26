b58ff5ec4eab12a6e0768aabd9e2cb1af8dccdc0
Rafael Gomes <rafaell.gomes@gmail.com>
2020-06-26 17:21:03 -0300

    Conclusão do Desafio 3-2

---
+++ modulo03/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "64c237ccff807c054339a62d53b4b402.svg",
+        title: "Starter",
+        duration: "5",
+        price: "Gratuito"
+    },
+    {
+        id: "0828532024cb46921a6b5e941f8d788d.svg",
+        title: "Launchbase",
+        duration: "30",
+        price: "Pago"
+    },
+    {
+        id: "64c237ccff807c054339a62d53b4b402.svg",
+        title: "GoStack",
+        duration: "40",
+        price: "Pago"
+    }
+]
