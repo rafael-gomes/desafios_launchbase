33f7e7c6e485e73e9b9eded40353c2c310c7d37d
Rafael Gomes <rafaell.gomes@gmail.com>
2020-06-26 18:52:03 -0300

    Remoção do Modal para abrir a página do curso

--- modulo03/public/scripts.js
+++ modulo03/public/scripts.js
@@ -3,28 +3,7 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
-        const siteId = card.getAttribute("id");
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${siteId}`;
+        const courseId = card.getAttribute("id");
+        window.location.href = `/course?id=${courseId}`
     })
 }
-
-document.querySelector('.close-modal').addEventListener("click", function(){
-    modalOverlay.classList.remove("active")
-    modalOverlay.classList.remove("maximize")
-    modalOverlay.querySelector("iframe").src = ""
-})
-
-document.querySelector('.maximize-modal').addEventListener("click", function(){
-  if(!modalOverlay.classList.contains('maximize')) {
-    modalOverlay.classList.add('maximize')
-    modalOverlay.querySelector("iframe").add('iframeMaximize')
-  }
-  else {
-    modalOverlay.classList.remove("maximize")
-  }
-})
-//Além do botão de fechar o modal, é preciso implementar a 
-//funcionalidade de maximizar o modal (dica: utilize a mesma lógica 
-//implementada para fechar o modalOverlay, mas trabalhe com a classe modal e 
-//utilize o método contains do classList para verificar se o elemento está ou não com a classe maximize).
